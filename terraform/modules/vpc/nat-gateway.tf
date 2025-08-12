fd2be963bc36c2cd3af6d7537d3cc4f43c042913
Vamsi-kms <vamsi.k@algorims.com>
2025-08-12 23:04:47 +0800

    initial commit

--- terraform/modules/vpc/nat-gateway.tf
+++ terraform/modules/vpc/nat-gateway.tf
@@ -1,19 +1,38 @@
+# resource "aws_eip" "nat" {
+#   count = var.create_nat_gateway && var.create_subnets ? length(aws_subnet.public) : 0
+
+#   tags = merge(var.tags, {
+#     Name = "${var.environment}-nat-eip-${count.index + 1}"
+#   })
+# }
+
+# resource "aws_nat_gateway" "this" {
+#   count = var.create_nat_gateway && var.create_subnets ? length(aws_subnet.public) : 0
+
+#   allocation_id = aws_eip.nat[count.index].id
+#   subnet_id     = aws_subnet.public[count.index].id
+#   depends_on    = [aws_internet_gateway.this]
+
+#   tags = merge(var.tags, {
+#     Name = "${var.environment}-nat-gateway-${count.index + 1}"
+#   })
+# }
+
 resource "aws_eip" "nat" {
-  count = var.create_nat_gateway && var.create_subnets ? length(aws_subnet.public) : 0
+  count = var.create_nat_gateway && var.create_subnets ? 1 : 0
 
   tags = merge(var.tags, {
-    Name = "${var.environment}-nat-eip-${count.index + 1}"
+    Name = "${var.environment}-nat-eip"
   })
 }
 
 resource "aws_nat_gateway" "this" {
-  count = var.create_nat_gateway && var.create_subnets ? length(aws_subnet.public) : 0
-
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = aws_subnet.public[count.index].id
+  count         = var.create_nat_gateway && var.create_subnets ? 1 : 0
+  allocation_id = aws_eip.nat[0].id
+  subnet_id     = aws_subnet.public[0].id
   depends_on    = [aws_internet_gateway.this]
 
   tags = merge(var.tags, {
-    Name = "${var.environment}-nat-gateway-${count.index + 1}"
+    Name = "${var.environment}-nat-gateway"
   })
 }
